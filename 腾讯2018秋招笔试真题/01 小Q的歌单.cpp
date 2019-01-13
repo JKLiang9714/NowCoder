9d7a8711f55034e1cd57b754d67c22c9b1180069
liangjiakun <1023587725@qq.com>
2019-01-13 15:39:54 +0800

    feature: tencent

--- 腾讯2018秋招笔试真题/01 小Q的歌单.cpp
+++ 腾讯2018秋招笔试真题/01 小Q的歌单.cpp
@@ -1,7 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
 using namespace std;
 
-int main() {
-	
+/*
+题解：从 a 中拿 i 首的个数 *  从 b 中拿 j 首的个数
+难点：排列组合，主要用二维数组来模拟计算组合数 C(n, m) 
+*/
+
+const int MAXN = 100;
+const int MOD = 1000000007;
+long long c[MAXN + 1][MAXN +1] = { 0 };
+
+// 存储组合数 C(n, m) 的值 
+void init() {
+	c[0][0] = 1;
+	for (int i = 1; i <= MAXN; i++) {
+		c[i][0] = 1;
+		for (int j = 1; j <= MAXN; j++) {
+			c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % MOD;
+		}
+	}
+}
+
+int main() 
+{
+	int k, a, x, b, y;
+	scanf("%d%d%d%d%d", &k, &a, &x, &b, &y);
+	init();
+	long long ans = 0;
+	for (int i = 0; i <= x; i++) {
+		// 对于 a 类歌曲，从 x 中取 i 个 
+		// 对于 b 类歌曲，从 y 中取 (k - i * a) / b 个
+		if (i * a <= k && (k - i * a) % b == 0 && (k - i * a) / b <= y) {
+			ans = (ans + (c[x][i] * c[y][(k - i * a) / b]) % MOD) % MOD;
+		}
+	} 
+	printf("%lld\n", ans);
 	return 0;
 }
+
+/*
+5
+2 3 3 3
+*/
