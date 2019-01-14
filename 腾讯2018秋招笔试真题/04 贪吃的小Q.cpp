a45ed8152382c49494f9101ef21a98c9d0f5198d
liangjiakun <1023587725@qq.com>
2019-01-14 10:45:06 +0800

    feature: tencent

---
+++ 腾讯2018秋招笔试真题/04 贪吃的小Q.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm> 
+using namespace std;
+
+/*
+题解：二分 
+*/
+int n, m;
+
+bool checkEnough(int num) {
+	int sumNum = m;
+	for (int i = 0; i < n; i++) {
+		sumNum -= num;
+		if (sumNum < 0) return false;
+		num = (num + 1) / 2;	// 注意不是num /= 2 
+	}
+	return true;
+}
+
+int main() 
+{
+	scanf("%d%d", &n, &m);
+	int l = 1, r = m;
+	while (l < r) {
+		int mid = (l + r) / 2;
+		int yes = checkEnough(mid);
+		if (yes) l = mid;
+		else r = mid - 1;
+	}
+	printf("%d\n", l);
+	return 0;
+}
+
+/*
+3 7
+*/
