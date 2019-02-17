bfe503eac33d9d540e2d396108f63bc2e264e5f7
liangjiakun <1023587725@qq.com>
2019-02-17 21:10:41 +0800

    feature: bytedance

--- 字节跳动2018秋招笔试真题/3矩阵游戏.cpp
+++ 字节跳动2018秋招笔试真题/3矩阵游戏.cpp
@@ -1,9 +1,47 @@
-#include <cstdio>
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
-int main()
-{
+int check(string const& str, int nums[4]) {
+    int cur_n = nums[2];
+    int cur_m = nums[3];
+    int step = 0;
+    for (int i = 0; i <= str.size(); i++) {
+        step++;
+        switch (str[i]) {
+        case 'u':
+            cur_n--;
+            if (cur_n < 1) return step;
+            break;
+        case 'd':
+            cur_n++;
+            if (cur_n > nums[0]) return step;
+            break;
+        case 'r':
+            cur_m++;
+            if (cur_m > nums[1]) return step;
+            break;
+        case 'l':
+            cur_m--;
+            if (cur_m < 1) return step;
+            break;
+        }
+    }
+    return step;
+}
 
+int main() {
+    string str;
+    cin >> str;
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int nums[4];
+        for (int j = 0; j != 4; j++) {
+            scanf("%d", &nums[j]);
+        }
+        int res = check(str, nums);
+        printf("%d\n", res);
+    }
     return 0;
 }
